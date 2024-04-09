f6e4ff50cade0a1cec05c449987162936ec7a71e
Dell <Dell@DESKTOP-0KQ1RUO>
2024-04-09 12:58:38 -0400

    Assignment03 by Shubhneet Kaur

--- Assignment03/src/Assignment3/Starter1.java
+++ Assignment03/src/Assignment3/Starter1.java
@@ -8,12 +8,12 @@ public class Starter1 {
 		int[] diceRolls = new int[20];
 		int totalRolls = 0;
 
-		// sample the rolls
+		
 		for(int count = 0; count < 1000; count++) {
 		    roll = (int)(Math.random() * 20) + 1;
 		    diceRolls[roll - 1] = diceRolls[roll - 1] + 1; // crashes
 		}	
-		// run a report
+		
 		for(int index = 0; index < diceRolls.length; index++) {
 		    System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
 		    totalRolls = totalRolls + diceRolls[index];

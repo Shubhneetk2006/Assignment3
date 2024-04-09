754baf74f5e248d8cb78978c4f6436019a7c6e44
Dell <Dell@DESKTOP-0KQ1RUO>
2024-04-09 13:01:20 -0400

    Assignment03 by SHUBHNEET KAUR DICEROLLS.JAVA

---
+++ Assignment03/src/Assignment3/Dicerolls.java
@@ -0,0 +1,27 @@
+package Assignment3;
+
+public class Dicerolls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				        int roll = 0;
+		        int[] diceRolls = new int[20];
+		        int totalRolls = 0;
+
+		       
+		        for (int count = 0; count < 1000; count++) {
+		            roll = (int) (Math.random() * 20) + 1;
+		            diceRolls[roll - 1] = diceRolls[roll - 1] + 1; // crashes
+		        }
+		        // run a report
+		        for (int index = 0; index < diceRolls.length; index++) {
+		            System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+		            totalRolls = totalRolls + diceRolls[index];
+		        }
+		        System.out.println("Total rolls were: " + totalRolls);
+		    }
+		
+
+	}
+
+
